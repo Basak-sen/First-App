78c22c64bac52789c50c8be7c6c3ec923de8b641
Basak-sen <basakaysegulsen@gmail.com>
2022-05-09 11:37:55 +0300

    The project is calculater for BMI

---
+++ lib/constant/app_constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart ';
+import 'package:hexcolor/hexcolor.dart';
+
+
+final Color mainHexColor = HexColor("#cccccc");
+final Color accentHexColor = HexColor("#1d6080");
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:homework2/screens/home.dart';
+import 'package:flutter/material.dart';
+import 'package:homework2/screens/app_home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Homework2 calculator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
